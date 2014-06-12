ad427ce07b611903c03075b07c982741495249c9
Dimitri Sokolyuk <demon@dim13.org>
2014-06-12 13:22:30 +0000

    don't move vertically if a viewport is smaller, then output

--- spectrogram.c
+++ spectrogram.c
@@ -335,6 +335,8 @@ move(Display *dsp, Window win, Window container)
 
 	dx = (wa.width - wac.width) / 2;
 	dy = (wa.height - wac.height) / 2;
+	if (dy < 0)
+		dy = 0;
 
 	XMoveWindow(dsp, container, dx, dy);
 
