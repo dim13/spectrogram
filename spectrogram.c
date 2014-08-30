850c0f4cd77ae94dc66130e6f01351d268f87b9c
Dimitri Sokolyuk <demon@dim13.org>
2014-08-30 19:53:03 +0000

    adjust colors

--- spectrogram.c
+++ spectrogram.c
@@ -285,7 +285,7 @@ init_panel(Display *d, Window win, int x, int y, int w, int h, int mirror)
 	p->mirror = mirror;
 
 	struct palette p_spectr = {
-		{ 0.30, 0.30, 0.50 },
+		{ 0.25, 0.80, 0.80 },
 		{ 0.00, 1.00, 1.00 }
 	};
 
@@ -294,8 +294,8 @@ init_panel(Display *d, Window win, int x, int y, int w, int h, int mirror)
 	free(palette);
 
 	struct palette p_shadow = {
-		{ 0.30, 0.30, 0.10 },
-		{ 0.00, 1.00, 0.20 }
+		{ 0.25, 0.80, 0.15 },
+		{ 0.00, 1.00, 0.25 }
 	};
 
 	palette = init_palette(d, &p_shadow, p->maxval, LIN_SCALE);
