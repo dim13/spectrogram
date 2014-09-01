f2736d91627fbbe879b9692d0dc98013a59fe655
Dimitri Sokolyuk <demon@dim13.org>
2014-09-01 18:25:26 +0000

    update hsl2rgb

--- hsl2rgb.c
+++ hsl2rgb.c
@@ -1,7 +1,23 @@
 /* $Id$ */
-
-/* input: H 0..360, S 0..100, L 0..100 */
-/* output: R, G, B 0..65535 */
+/*
+ * Copyright (c) 2014 Dimitri Sokolyuk <demon@dim13.org>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+/*
+ * input: H 0..360, S 0..100, L 0..100
+ * output: R, G, B 0..65535
+ */
 
 void
 hsl2rgb(unsigned short *r, unsigned short *g, unsigned short *b,
