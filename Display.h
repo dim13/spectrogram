2e27e99d1992efc72bc186b8d0b679266f276e6c
Dimitri Sokolyuk <demon@dim13.org>
2014-09-30 20:49:32 +0000

    add two new resources for future use

--- Display.h
+++ Display.h
@@ -6,6 +6,12 @@
 #define XtNdata		"data"
 #define XtCData		"Data"
 
+#define XtNround	"round"
+#define XtCRound	"Round"
+
+#define XtNmaxRound	"maxRound"
+#define XtCMaxRound	"MaxRound"
+
 typedef struct _DisplayClassRec *DisplayWidgetClass;
 typedef struct _DisplayRec *DisplayWidget;
 
